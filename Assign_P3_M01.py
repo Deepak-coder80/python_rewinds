43559881d95dbfe7fa28f9dc57b25946768c1d29
Deepak M S <souparnikad40@gmail.com>
2021-10-17 18:35:16 +0530

    problem 3  question added

---
+++ Assign_P3_M01.py
@@ -0,0 +1,8 @@
+'''
+Illustrate the Creation of a Dictionary using lists with suitable code.
+Consider the example dictionary d={1:10, 2:20, 3:30, 4:40, 5:50},
+all the key and values to be read from user and store it in 2 lists
+named “keys” and “values”. Further create the dictionary from
+these 2 lists and display the items accordingly
+'''
+
